ca3e60092ddfe195380d7dd9b627b8c39feca62a
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-08-31 14:38:21 +1000

    80% done

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -12,6 +12,9 @@ public class TimeManager : MonoBehaviour
 
     const float moveWait = 2.0f;
 
+    public GameObject red;
+    public GameObject blue;
+
     Camera cam;
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,10 @@ public class TimeManager : MonoBehaviour
         if(timer >= lastTime)
         {
             Debug.Log(lastTime);
+
+            if(lastTime%2 == 0)
+            MoveObjects();
+
             lastTime++;
         }
 
@@ -60,5 +67,32 @@ public class TimeManager : MonoBehaviour
     {
         timer = 0;
         lastTime = 0;
+    }
+
+    private void MoveObjects()
+    {
+
+    Vector3 pos1 = new Vector3(2, 1, 0);
+    Vector3 pos2 = new Vector3(2, -1, 0);
+    Vector3 pos3 = new Vector3(-2, -1, 0);
+    Vector3 pos4 = new Vector3(-2, 1, 0);
+
+        if(red.transform.position == pos1)
+            red.transform.position = pos2;
+        else if(red.transform.position == pos2)
+            red.transform.position = pos3;
+        else if(red.transform.position == pos3)
+            red.transform.position = pos4; 
+        else if(red.transform.position == pos4)
+            red.transform.position = pos1;
+    
+        if(blue.transform.position == pos1)
+            blue.transform.position = pos2;
+        else if(blue.transform.position == pos2)
+            blue.transform.position = pos3;
+        else if(blue.transform.position == pos3)
+            blue.transform.position = pos4;
+        else if(blue.transform.position == pos4)
+            blue.transform.position = pos1;
     }
 }
