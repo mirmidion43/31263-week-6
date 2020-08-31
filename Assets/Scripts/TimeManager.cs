fe4be022cd322d0ae3a47ab9dc8700e985906431
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-08-31 13:08:02 +1000

    40% finished

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private int lastTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTime = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > lastTime)
+        {
+            Debug.Log(lastTime);
+            lastTime++;
+        }
+
+    }
+}
